bdb837f6e6a2fe7c263ddf23363631db0bdabe93
subhadippal66 <62802794+subhadippal66@users.noreply.github.com>
2020-09-08 09:40:10 +0530

    File_Added

---
+++ Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var moveX = Input.GetAxis("Horizontal") * Time.deltaTime *moveSpeed;
+        var moveY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newXpos = transform.position.x + moveX;
+        var newYpos = transform.position.y + moveY;
+        transform.position = new Vector2(newXpos, newYpos);
+    }
+}

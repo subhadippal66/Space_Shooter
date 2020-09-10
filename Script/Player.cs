599e03eed5766ad8945d493e53697077157c3bf4
subhadippal66 <62802794+subhadippal66@users.noreply.github.com>
2020-09-10 16:08:24 +0530

    Feature Added_001
    fire added
    movement resticted using viewporttoworldunits()

--- Script/Player.cs
+++ Script/Player.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //configuration parameter
     [SerializeField] float moveSpeed;
+    [SerializeField] float xBoundariesOffset;
+    [SerializeField] float yBoundariesOffset;
+    [SerializeField] GameObject laser_;
+    [SerializeField] float fireSpeed;
+    [SerializeField] float laserOffset;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
     
     void Start()
     {
-        
+        DefineBoundaries();
     }
 
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Vector2 laserSpawnPos = new Vector2(transform.position.x, transform.position.y + laserOffset);
+            GameObject laser_0 = Instantiate(laser_, laserSpawnPos, Quaternion.identity) as GameObject;
+            laser_0.GetComponent<Rigidbody2D>().velocity = new Vector2(0, fireSpeed);
+            Destroy(laser_0, 2f);
+        }
+    }
+
+    private void DefineBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xBoundariesOffset;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xBoundariesOffset;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + yBoundariesOffset;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yBoundariesOffset;
     }
 
     private void Move()
     {
         var moveX = Input.GetAxis("Horizontal") * Time.deltaTime *moveSpeed;
         var moveY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXpos = transform.position.x + moveX;
-        var newYpos = transform.position.y + moveY;
+        var newXpos = Mathf.Clamp(transform.position.x + moveX, xMin, xMax);
+        var newYpos = Mathf.Clamp(transform.position.y + moveY, yMin, yMax);
         transform.position = new Vector2(newXpos, newYpos);
     }
 }
